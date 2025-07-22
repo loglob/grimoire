f6ac6a1ccb29ed08665320ecd5e276a9d5796234
loglob <23386237+loglob@users.noreply.github.com>
2025-07-22 03:56:52 +0200

    Track uncertain materials  UI needs more work

--- website/UI/Materials.ts
+++ website/UI/Materials.ts
@@ -6,49 +6,73 @@ namespace UI
 	import IGame = Games.IGame;
 	import IMaterialContext = Games.IMaterialContext;
 
-	type PriceSum = Readonly<{ value: number, unsure: boolean }>
-
-	const ZERO : PriceSum = { value: 0, unsure: false }
-
-	function sumPrices(...xs : (number | null)[]) : PriceSum
+	class Price
 	{
-		var sum = 0;
-		var unsure = false;
+		public readonly value : number
+		public readonly unsureAbout : readonly string[]
 
-		for (const x of xs)
+		public get unsure() : boolean
 		{
-			if(x === null)
-				unsure = true;
-			else
-				sum += x;
+			return this.unsureAbout.length > 0
 		}
 
-		return { value: sum, unsure: unsure };
-	}
+		private constructor(value : number, unsureAbout : readonly string[])
+		{
+			this.value = value;
+			this.unsureAbout = unsureAbout
+		}
 
-	function scale(p : PriceSum, x : number) : PriceSum
-	{
-		return { value: p.value * x, unsure: p.unsure }
-	}
+		public static Certain(value : number) : Price
+		{
+			return new Price(value, [])
+		}
 
-	function sum(a : PriceSum, ...bs : PriceSum[])
-	{
-		for (const b of bs)
-			a = { value: a.value + b.value, unsure: a.unsure || b.unsure }
+		public static Unsure(...about : readonly string[])
+		{
+			return new Price(0.0, about)
+		}
 
-		return a
+		public static readonly ZERO : Price = new Price(0, [])
+
+		public plus(other : Price) : Price
+		{
+			return new Price( this.value + other.value, this.unsureAbout.concat(other.unsureAbout) );
+		}
+
+		public scale(by : number) : Price
+		{
+			if(by == 0)
+				return Price.ZERO;
+
+			return new Price( this.value * by, this.unsureAbout );
+		}
+
+		public static sum(...xs : Price[])
+		{
+			var acc = this.ZERO
+
+			for(const x of xs)
+				acc = acc.plus(x);
+
+			return acc;
+		}
+
+		public static of(m : Data.IMaterial)
+		{
+			return m.price === null ? this.Unsure(m.display) : this.Certain(m.price);
+		}
 	}
 
 	/** State container for observing  */
 	class SumGadget<TSpell extends Data.ISpell, TMaterial extends Data.IMaterial>
 	{
 		readonly parent : Materials<TSpell, TMaterial>;
-		public readonly consumed : PriceSum;
-		public readonly persistent : PriceSum;
+		public readonly consumed : Price;
+		public readonly persistent : Price;
 		readonly input : HTMLInputElement;
 		castCount : number = 0;
 
-		constructor(parent : Materials<TSpell, TMaterial>, persistent : PriceSum, consumed : PriceSum, input : HTMLInputElement)
+		constructor(parent : Materials<TSpell, TMaterial>, persistent : Price, consumed : Price, input : HTMLInputElement)
 		{
 			this.parent = parent;
 			this.persistent = persistent;
@@ -100,13 +124,13 @@ namespace UI
 			this.recomputeTotals()
 		}
 
-		formatPriceSum(p : PriceSum) : (string|Node)[]
+		formatPriceSum(p : Price) : (string|Node)[]
 		{
-			if(p.unsure && p.value == 0)
+			if(p.unsureAbout.length && p.value == 0)
 				return ["???"]
 
 			const price = this.context.formatPrice(p.value)
-			return p.unsure ? [price, "+?"] : [price]
+			return p.unsureAbout.length ? [price, "+?"] : [price]
 		}
 
 		formatSpells(table : HTMLTableElement, spells : TSpell[])
@@ -124,12 +148,12 @@ namespace UI
 
 				child(row, "td").appendChild(this.context.formatMaterials(persistent, true))
 
-				const pSum = sumPrices(...persistent.map(m => m.price))
+				const pSum = Price.sum(...persistent.map(m => Price.of(m)))
 				child(row, "td").append(...this.formatPriceSum(pSum))
 
 				child(row, "td").appendChild(this.context.formatMaterials(consumed, true))
 
-				const cSum = sumPrices(...consumed.map(m => m.price))
+				const cSum = Price.sum(...consumed.map(m => Price.of(m)))
 				child(row, "td").append(... this.formatPriceSum(cSum))
 
 				const inp = child(child(row, "td"), "input") as HTMLInputElement
@@ -141,13 +165,47 @@ namespace UI
 			}
 		}
 
+		formatUnsure(sum : Price) : (string | Node)[]
+		{
+			// TODO: turn this into a table & maybe parse out the actual material
+			if(! sum.unsure)
+				return [];
+
+			var result = [];
+
+			result.push(
+				document.createElement("br"),
+				`Materials without listed prices: `
+			);
+
+			var first = true;
+
+			for (const m of sum.unsureAbout)
+			{
+				if(! first)
+				{
+					const sep = document.createElement("span");
+					sep.className = "material-sep";
+					sep.innerText = ",";
+					result.push(sep);
+				}
+
+				first = false;
+				const txt = document.createElement("span");
+				txt.innerHTML = m;
+				result.push(txt);
+			}
+
+			return result;
+		}
+
 		/** regenerates the totals displayed under the table */
 		recomputeTotals()
 		{
 			var totalCasts = 0
 			var totalSpells = 0
-			var persistentSum = ZERO
-			var consumedSum = ZERO
+			var persistentSum = Price.ZERO
+			var consumedSum = Price.ZERO
 
 			for(const g of this.gadgets)
 			{
@@ -156,8 +214,8 @@ namespace UI
 				if(g.castCount > 0)
 				{
 					++totalSpells;
-					persistentSum = sum(persistentSum, g.persistent);
-					consumedSum = sum(consumedSum, scale(g.consumed, g.castCount))
+					persistentSum = persistentSum.plus(g.persistent);
+					consumedSum = consumedSum.plus(g.consumed.scale(g.castCount));
 				}
 			}
 
@@ -168,13 +226,15 @@ namespace UI
 
 			consumedContainer.append(
 				`Material cost for all ${totalCasts} casting${totalCasts != 1 ? 's' : ''} is `,
-				Util.wrap("b", ...this.formatPriceSum(consumedSum))
+				Util.wrap("b", ...this.formatPriceSum(consumedSum)),
+				// ... this.formatUnsure(consumedSum)
 			);
 
 			persistentContainer.append(
 				`Casting all ${totalSpells} selected spell${totalSpells != 1 ? 's' : ''} requires an additional `,
 				Util.wrap("b", ...this.formatPriceSum(persistentSum)),
-				` in persistent components`
+				` in persistent components`,
+				// ... this.formatUnsure(persistentSum)
 			)
 		}
 	}
